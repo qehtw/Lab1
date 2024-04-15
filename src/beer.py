3c1133e70aeff26ade24835b2507f03fc6e940ed
qehtw <maxgrynkiv@gmail.com>
2024-04-15 18:21:21 +0300

    add lab 6

---
+++ src/beer.py
@@ -0,0 +1,27 @@
+def beers(number_of_workers, beer_types, workers_loved_beer):
+    workers_loved_beer = workers_loved_beer.split()
+    types_of_beer = 0
+    beers_sett = []
+
+    for i in range(number_of_workers):
+        possible_sett = []
+
+        for j in range(beer_types):
+            if workers_loved_beer[i][j] == 'Y':
+               possible_sett.append(j)
+
+        if len(possible_sett) == beer_types:
+            continue
+
+        else:
+            if any(item in beers_sett for item in possible_sett) == False:
+                beers_sett.extend(possible_sett)
+                types_of_beer += 1
+
+    if types_of_beer == 0:
+        return 1
+    
+    else:
+        return types_of_beer
+    
+print(beers(number_of_workers= 2 , beer_types= 3 , workers_loved_beer= 'NYY NNY'))  
---
+++ test/test_beer.py
@@ -0,0 +1,23 @@
+import unittest
+from src.beer import *
+
+class Test_some_methods(unittest.TestCase):
+    
+    def test_beers(self):
+        self.assertEqual(beers(2, 2, 'YN NY'),2)
+
+    def test2_beers(self):
+        self.assertEqual(beers(6, 3, 'YYY YYY YYY YYY YYY YYY'),1)
+
+    def test3_beers(self):
+        self.assertEqual(beers(6, 3, 'YNN YNY YNY NYY NYY NYN'),2)
+
+    def test4_beers(self):
+        self.assertEqual(beers(4, 1, "Y Y Y Y"), 1)
+
+    def test5_beers(self):
+        self.assertEqual(beers(4, 1, "YN YYY YN YN"), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
