7ab23ba1186c363fb1ef7fb712c18039200eb846
qehtw <maxgrynkiv@gmail.com>
2024-05-17 10:45:26 +0300

    some fixes

--- src/elecrt.py
+++ src/elecrt.py
@@ -1,8 +1,8 @@
 import os
 import math
 
-def read_input(file_name):
 
+def read_input(file_name):
     """
     This function reads info from a txt file.
     """
@@ -18,70 +18,72 @@ def read_input(file_name):
 
     return input_info
 
-def is_closer_to(current_pillar_heigth , next_pillar_heigth):
-
-    diff1 = abs(current_pillar_heigth - 1)
-    diff2 = abs(current_pillar_heigth - next_pillar_heigth)
-
-    if diff1 < diff2:
-        return 1
-    else: 
-        if diff2 < diff1:
-            return next_pillar_heigth
-        else: 
-            return False
-
-
 
 def data_check(distance, pillars):
-
     """
     This function checks if the data is true to the terms
     """
 
-    if distance > 100 or distance < 1 or len(pillars) > 50 :
+    if distance > 100 or distance < 1 or len(pillars) > 50:
         return False
     for i in range(0, len(pillars)):
         if pillars[i] > 100 or pillars[i] < 1:
             print(pillars[i])
             return False
 
-def find_length_of_cabel(distance,pillars):
-    
+
+def check_closer(next_heigth, current_heigth):
+
+    target = 1
+
+    if abs(current_heigth - next_heigth) < abs(current_heigth - target):
+        return 1
+
+
+def calculate_worst_scenario(pillars):
+    """
+    This function creates worst possible heights of pillars
+    """
+    i = 0
+    new_heigths_of_pillars = pillars
+
+    for i in range(0, len(pillars) - 1):
+        if new_heigths_of_pillars[i] > new_heigths_of_pillars[i + 1]:
+            new_heigths_of_pillars[i + 1] = 1
+            continue
+
+        if new_heigths_of_pillars[i] == 1:
+            continue
+
+        if check_closer(new_heigths_of_pillars[i + 1], new_heigths_of_pillars[i]) == 1:
+            new_heigths_of_pillars[i + 1] = 1
+            continue
+
+    return new_heigths_of_pillars
+
+
+def find_length_of_cabel(distance, new_pillars):
     """
     This function calculates lengths of cabel that we might need
     """
 
     total_length = 0
-    number_of_pillars = len(pillars)
-    current_pillar_heigth = pillars[0]
-    next_pillar_heigth = pillars[1]
-    for i in range(0,number_of_pillars-1):
-        if is_closer_to(current_pillar_heigth , next_pillar_heigth) == next_pillar_heigth or next_pillar_heigth == current_pillar_heigth :
-            next_pillar_heigth = 1
-            length = math.sqrt((current_pillar_heigth - next_pillar_heigth )**2 + distance**2)
-            current_pillar_heigth = 1
-            next_pillar_heigth = pillars[i]
-            total_length += length
-        else:
-            if current_pillar_heigth == 1 or is_closer_to(current_pillar_heigth, next_pillar_heigth) == 1:
-                length = math.sqrt((current_pillar_heigth - next_pillar_heigth )**2 + distance**2)
-                current_pillar_heigth = next_pillar_heigth
-                next_pillar_heigth = pillars[i+1]
-                total_length += length
-            else:
-                return 2
-    return round(total_length , 2)
+    number_of_pillars = len(new_pillars)
+    for i in range(0, number_of_pillars - 1):
+        length = math.sqrt((new_pillars[i] - new_pillars[i + 1]) ** 2 + distance**2)
+        total_length += length
+    return round(total_length, 2)
 
-def start_code(file_name):
-    
+
+def start_code():
     """
     This function start code
     """
 
-    data_from_file = read_input(file_name)
+    data_from_file = read_input("electr_input.txt")
     distance = data_from_file[0][0]
     pillars = data_from_file[1]
     if data_check(distance, pillars) == False:
         return False
-    return find_length_of_cabel(distance, pillars)
+    new_pillars = calculate_worst_scenario(pillars)
+    return find_length_of_cabel(distance, new_pillars)
